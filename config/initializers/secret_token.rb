173dfe7cbefd1cca2942ad47bc0748df8084b691
Nixers <nixers@fixrnix.in>
2013-07-16 12:22:02 +0530

    first-commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Policy::Application.config.secret_token = '83a4b6d6944432ab21c8c2097f6085c5184459c05c01974830598ff045dbc4ffcc4d119aa0b4d1e40aed8527ab3881b41b827015feb80319c6f69dd89d147663'
